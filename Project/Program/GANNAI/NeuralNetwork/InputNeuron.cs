528ea3099d164a0135730969a34f4d4ef93d62aa
Elias Obeid <eobeid11@student.aau.dk>
2014-02-20 19:48:18 +0100

    finished implementing the neural network. editing lines in kent's code to fix how Utility.random is used. removed Main methods from projects. this gave errors. should we have main methods for every single project?

--- Project/Program/GANNAI/NeuralNetwork/InputNeuron.cs
+++ Project/Program/GANNAI/NeuralNetwork/InputNeuron.cs
@@ -2,13 +2,22 @@
 using System.Text;
 using System.Collections.Generic;
 
-namespace NeuralNetwork
-{
-	public class InputNeuron : Neuron
-	{
-		public InputNeuron ()
-		{
-		}
+namespace ArtificialNeuralNetwork {
+	public class InputNeuron : Neuron {
+    //list of connections from this neuron
+    //input neurons only have outgoing connections
+    private List<Connection> outputs = new List<Connection>();
+
+    //the value of the input
+    private double value;
+    //set the input value
+    public void setValue(double value){
+      this.value = value;
+    }
+    //return the input value
+    public double calculate(){
+      return value;
+    }
 	}
 }
 
