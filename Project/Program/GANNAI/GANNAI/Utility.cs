3e68f686b486c261cae43716a4cb82ce8a80dbec
Elias Obeid <eobeid11@student.aau.dk>
2014-02-20 17:32:38 +0100

    working on neural network. added Utility class.

---
+++ Project/Program/GANNAI/GANNAI/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace GANNAI {
+	public static class Utility {
+		//random operator
+		private static Random random = new Random();
+
+		//return random integer
+		public static int RandomInt(int start, int end) {
+			return random.Next(start, end);
+		}
+
+		//return random double
+		public static double RandomDouble(){
+			return random.NextDouble(); 
+		}
+
+		//return random boolean
+		public static bool RandomBool(){
+			return (random.NextDouble() >= 0.5) ? true : false;
+		}
+	}
+}
+
