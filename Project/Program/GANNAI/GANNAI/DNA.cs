0b8b1c4d48b99ac78b53b07dc22cffa163441da8
Kent Caspersen <kcaspe11@student.aau.dk>
2014-02-20 11:12:18 +0100

    tilføjet projektets kode

---
+++ Project/Program/GANNAI/GANNAI/DNA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GANNAI {
+  public class DNA {
+    private bool[] bitstring;
+    public DNA(int length) {
+      bitstring = new bool[length];
+      Random random = new Random();
+      for (int i = 0; i < length; i++)
+        bitstring[i] = random.Next(0, 2) == 1 ? true : false;
+    }
+  }
+}
