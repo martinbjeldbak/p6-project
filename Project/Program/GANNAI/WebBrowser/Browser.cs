cc3b3952ba1dfb3fc044f1aed25368c5652b5afb
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-02-21 14:14:03 +0100

    Load pacman and add support for moving around in game

--- Project/Program/GANNAI/WebBrowser/Browser.cs
+++ Project/Program/GANNAI/WebBrowser/Browser.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-namespace WebBrowser
-{
-	public class Browser
-	{
-		public Browser()
-		{
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+
+namespace WebBrowser {
+	public class Browser {
+    protected IWebDriver driver;
+
+		public Browser() {
+      driver = new PhantomJSDriver();
+      driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
 		}
+
+    public void quit() {
+      driver.Quit();
+      driver.Dispose();
+    }
 	}
 }
 
