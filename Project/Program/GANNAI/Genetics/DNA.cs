b6d315145b662a47b08efa5da0c62cd65bea3c6c
Kent Caspersen <kcaspe11@student.aau.dk>
2014-02-21 13:33:02 +0100

    ny projektstruktur

--- Project/Program/GANNAI/Genetics/DNA.cs
+++ Project/Program/GANNAI/Genetics/DNA.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using GANNAI;
 
-namespace GANNAI {
+namespace Genetics {
   public class DNA {
+    private static double mutationRate = 0.01;
     private bool[] bitstring;
     private int hashcode;
 
     //Constructs a random DNA string matching the length of the neural network architecthure we need
     public DNA() : this(100){
-      //TODO: How long is such a string?
     }
 
-    //Constructs a random DNA string of a given length
+    /// <summary>
+    /// Constructs a random DNA string of a given length
+    /// </summary>
+
     public DNA(int length) {
       bitstring = new bool[length];
       for(int i = 0; i < length; i++)
@@ -22,12 +24,20 @@ namespace GANNAI {
       hashcode = GetHashCode();
     }
 
-    //Construct a DNA string with the given bitstring
+    /// <summary>
+    /// Construct a DNA string with the given bitstring
+    /// </summary>
     public DNA(bool[] bitstring) {
       this.bitstring = bitstring;
     }
 
-    public DNA SinglePointCrossover(DNA other) {
+
+    /// <summary>
+    /// Performs single point crossover between itself and another DNA string
+    /// </summary>
+    /// <param name="other">Other DNA string</param>
+    /// <returns>A new DNA string</returns>
+    public DNA GetSinglePointCrossover(DNA other) {
       if (bitstring.Length != other.bitstring.Length)
         throw new Exception("The two bitstrings to be crossed must have the same length.");
 
@@ -49,7 +59,13 @@ namespace GANNAI {
       return new DNA(result);
     }
 
-    public DNA TwoPointCrossover(DNA other) {
+
+    /// <summary>
+    /// Performs two point crossover between itself and another DNA string
+    /// </summary>
+    /// <param name="other">Other DNA string</param>
+    /// <returns>A new DNA string</returns>
+    public DNA GetTwoPointCrossover(DNA other) {
       if (bitstring.Length != other.bitstring.Length)
         throw new Exception("The two bitstrings must have same length to be crossed");
       bool[] result = new bool[bitstring.Length];
@@ -72,7 +88,13 @@ namespace GANNAI {
         result[i] = left[i];
       return new DNA(result);
     }
-    public DNA UniformCrossover(DNA other) {
+
+    /// <summary>
+    /// Performs uniform crossover between itself and another DNA string
+    /// </summary>
+    /// <param name="other">Other DNA string</param>
+    /// <returns>A new DNA string</returns>
+    public DNA GetUniformCrossover(DNA other) {
       if (bitstring.Length != other.bitstring.Length)
         throw new Exception("The two bitstrings must have same length to be crossed");
       bool[] result = new bool[bitstring.Length];
@@ -101,13 +123,26 @@ namespace GANNAI {
       return true;
     }
 
-    //Returns a deep copy of itself
+    /// <summary>
+    /// Returns a clone of itself
+    /// </summary>
     public DNA Clone() {
       bool[] clonedBitstring = new bool[bitstring.Length];
       for (int i = 0; i < bitstring.Length; i++)
         clonedBitstring[i] = bitstring[i];
       DNA clone = new DNA(clonedBitstring);
       return clone;
+    }
+
+    /// <summary>
+    /// Returns a mutated copy of 
+    /// </summary>
+    public DNA GetMutated() {
+      DNA result = Clone();
+      for (int i = 0; i < bitstring.Length; i++)
+        if (Utility.randomDouble() < mutationRate)
+          bitstring[i] = !bitstring[i];
+      return result;
     }
 
     public override int GetHashCode() {
--- Project/Program/GANNAI/Tests/Properties/AssemblyInfo.cs
+++ Project/Program/GANNAI/Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f86989c-2cbe-4077-939c-413c702f7c41")]
+[assembly: Guid("102b594f-10b2-447f-81da-0a9422886342")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
