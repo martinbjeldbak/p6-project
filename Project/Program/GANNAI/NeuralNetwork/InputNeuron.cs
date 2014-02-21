00eedb358f3b67d129ca137924dc43f0a178a2e9
Elias Obeid <eobeid11@student.aau.dk>
2014-02-21 13:11:14 +0100

    added test projects.

--- Project/Program/GANNAI/NeuralNetwork/InputNeuron.cs
+++ Project/Program/GANNAI/NeuralNetwork/InputNeuron.cs
@@ -3,21 +3,21 @@ using System.Text;
 using System.Collections.Generic;
 
 namespace ArtificialNeuralNetwork {
-  public class InputNeuron : Neuron {
+	public class InputNeuron : Neuron {
     //list of connections from this neuron
     //input neurons only have outgoing connections
     private List<Connection> outputs = new List<Connection>();
 
-    //the value of the input
-    private double value;
+    //neuron value
+    public double Value { get; private set; }
     //set the input value
-    public void SetValue(double value) {
-      this.value = value;
+    public void SetValue(double value){
+      this.Value = value;
     }
     //return the input value
-    public double GetValue() {
-      return value;
+    public double Calculate(){
+      return Value;
     }
-  }
+	}
 }
 
---
+++ Project/Program/GANNAI/TestArtificialNeuralNetwork/NeuronTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace GANNAINTests {
+  [TestFixture()]
+  public class NeuronTest {
+    [Test()]
+    public void AddInputConnectionTest() {
+
+    }
+    [Test()]
+    public void ActivationFunctionTest() {
+
+    }
+    [Test()]
+    public void CalculateTest() {
+
+    }
+  }
+}
+
