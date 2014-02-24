ddab098ff6d5b96f6474b6ede974bd7aee219d7e
Elias Obeid <eobeid11@student.aau.dk>
2014-02-24 14:32:01 +0100

    Unit tests for Neural Network are up and running.

--- Project/Program/GANNAI/GANNAITests/NeuralNetworkTest.cs
+++ Project/Program/GANNAI/GANNAITests/NeuralNetworkTest.cs
@@ -5,58 +5,27 @@ using ArtificialNeuralNetwork;
 namespace GANNAINTests {
   [TestFixture()]
   public class ArtificialNeuralNetworkTest {
-    [Test()]
-    public void BasicStructureTest() {
-      NeuralNetwork nn = new NeuralNetwork(1, 2, 2);
-      Assert.AreEqual(1, nn.InputNeurons.Count);
-      Assert.AreEqual(2, nn.HiddenNeurons.Count);
-      Assert.AreEqual(2, nn.OutputNeurons.Count);
-
-      NeuralNetwork nn1 = new NeuralNetwork(100, 200, 200);
-      Assert.AreEqual(100, nn1.InputNeurons.Count);
-      Assert.AreEqual(200, nn1.HiddenNeurons.Count);
-      Assert.AreEqual(200, nn1.OutputNeurons.Count);
-    }
-
-    [Test()]
-    public void InputTest() {
-      NeuralNetwork nn = new NeuralNetwork(2, 3, 2);
-      double[] v = { 0.0, 0.1 };
-
-      nn.SetInput(v);
-      nn.GetOutput();
-
-      for(int i = 0; i < v.Length; i++) {
-        Assert.AreEqual(v[i], nn.InputNeurons[i].Value, 0.0);
-      }
-    }
 
     [Test()]
     public void CalculationTest() {
-      NeuralNetwork nn = new NeuralNetwork(2, 3, 2);
-      double[] v = {0.2, 0.2};
-      nn.SetInput(v); 
+      double[] weights = new double[(2*3)+(3*2)];
+      for(int i = 0; i < (2*3)+(3*2); i++)
+        weights[i] = 1.0;
 
-      for(int i = 0; i < nn.HiddenNeurons.Count; i++)
-        for(int j = 0; j < nn.InputNeurons.Count; j++)
-          nn.HiddenNeurons[i].AddInputConnection(nn.InputNeurons[j], 1.0);       
-      for(int i = 0; i < nn.OutputNeurons.Count; i++)
-        for(int j = 0; j < nn.HiddenNeurons.Count; j++)
-          nn.OutputNeurons[i].AddInputConnection(nn.HiddenNeurons[j], 1.0);
+      NeuralNetwork nn = new NeuralNetwork(2, 3, 2, weights);
+
+      double[] inputVector = {0.2, 0.2};
+      nn.SetInput(inputVector);
+      
       //first hidden values then output values
-      double resultHidden = 1 / (1 + Math.Exp(-(0.2 * 1.0 + 0.2 * 1.0)));
-      double resultOutput = 1 / (1 + Math.Exp(-(resultHidden * 1.0 + resultHidden * 1.0 + resultHidden * 1.0)));
+      double result = 1 / (1 + Math.Exp(-(0.2 * 1.0 + 0.2 * 1.0)));
+      result = 1 / (1 + Math.Exp(-(result * 1.0 + result * 1.0 + result * 1.0)));
 
-      nn.GetOutput();
+      double[] outputVector = nn.GetOutput();
+      Assert.AreEqual(2, outputVector.Length);
 
-      for(int i = 0; i < nn.InputNeurons.Count; i++)
-        Assert.AreEqual(v[i], nn.InputNeurons[i].Value, 0.0);
-
-      for(int i = 0; i < nn.HiddenNeurons.Count; i++)
-        Assert.AreEqual(resultHidden, nn.HiddenNeurons[i].Value, 0.0);
-
-      for(int i = 0; i < nn.OutputNeurons.Count; i++)
-        Assert.AreEqual(resultOutput, nn.OutputNeurons[i].Value, 0.0);
+      for(int i = 0; i < 2; i++)
+        Assert.AreEqual(result, outputVector[i], 0.0);
 
       /*
       nn.setInputVector(0.1, 0.4);
--- Project/Program/GANNAI/NeuralNetwork/NeuralNetwork.cs
+++ Project/Program/GANNAI/NeuralNetwork/NeuralNetwork.cs
@@ -9,48 +9,77 @@ namespace ArtificialNeuralNetwork {
     private List<ChildNeuron> hiddenNeurons;
     private List<ChildNeuron> outputNeurons;
 
-    public NeuralNetwork(int inp, int hdn, int oup, int l = 1){
+    public NeuralNetwork(int inp, int hdn, int oup) {
+      InitiateNetwork(inp, hdn, oup);
+      AddConnections();
+    }
+
+    public NeuralNetwork(int inp, int hdn, int oup, double[] weights) {
+      InitiateNetwork(inp, hdn, oup);
+      AddConnections(weights);
+    }
+
+    private void InitiateNetwork(int inp, int hdn, int oup){
       inputNeurons = new List<InputNeuron>();
       hiddenNeurons = new List<ChildNeuron>();
       outputNeurons = new List<ChildNeuron>();
 
       //validation
       if(inp < 0)
-        throw new Exception("Must have at least one input neuron!" 
+        throw new Exception("Must have at least one input neuron!"
           + inp + " input neurons where given.");
       if(hdn < 0)
-        throw new Exception("Must have at least one hidden neuron!" 
+        throw new Exception("Must have at least one hidden neuron!"
           + hdn + " hidden neurons where given.");
       if(oup < 0)
-        throw new Exception("Must have at least one output neuron!" 
+        throw new Exception("Must have at least one output neuron!"
           + oup + " output neurons where given.");
-      if(l < 1)
-        throw new Exception("Incorrect number of layers. Must be greater than zero!" 
-          + l + " layers where given.");
 
       //add input neurons to network
       for(int i = 0; i < inp; i++)
         AddInput(new InputNeuron());
-
       //add hidden neurons to network
-      //take layers (l) into account
-      if(l == 1) {
-        for(int i = 0; i < hdn; i++)
-          AddHidden(new ChildNeuron());
-      }
-      else {
-        //TODO: How to we handle multiple layers?
-        for(int j = 0; j < l; j++) {
-          for(int i = 0; i < hdn; i++)
-            AddOutput(new ChildNeuron());
-        }
-      }
-
+      for(int i = 0; i < hdn; i++)
+        AddHidden(new ChildNeuron());
       //add output neurons to network
       for(int i = 0; i < oup; i++)
-          AddOutput(new ChildNeuron());
+        AddOutput(new ChildNeuron());
     }
 
+    private void AddConnections(){
+      //connect network (input to hidden)
+      for(int i = 0; i < hiddenNeurons.Count; i++) {
+        for(int j = 0; j < inputNeurons.Count; j++) {
+          hiddenNeurons[j].AddInputConnection(inputNeurons[i]);
+        }
+      }
+      //connect network (hidden to output)
+      for(int i = 0; i < outputNeurons.Count; i++) {
+        for(int j = 0; j < hiddenNeurons.Count; j++) {
+          outputNeurons[i].AddInputConnection(hiddenNeurons[j]);
+        }
+      }
+    }
+
+    private void AddConnections(double[] weights) {
+      if(weights.Length != (inputNeurons.Count * hiddenNeurons.Count) 
+        + (hiddenNeurons.Count * outputNeurons.Count))
+        throw new Exception("Number of weights do not correspond to number of connections.");
+
+      //connect network (input to hidden)
+      int weightIndex = 0;
+      for(int i = 0; i < hiddenNeurons.Count; i++) {
+        for(int j = 0; j < inputNeurons.Count; j++) {
+          hiddenNeurons[i].AddInputConnection(inputNeurons[j], weights[weightIndex++]);
+        }
+      }
+      //connect network (hidden to output)
+      for(int i = 0; i < outputNeurons.Count; i++) {
+        for(int j = 0; j < hiddenNeurons.Count; j++) {
+          outputNeurons[i].AddInputConnection(hiddenNeurons[j], weights[weightIndex++]);
+        }
+      }
+    }
     //add input neuron
     public void AddInput(InputNeuron neuron) {
       inputNeurons.Add(neuron);
@@ -63,19 +92,17 @@ namespace ArtificialNeuralNetwork {
     public void AddOutput(ChildNeuron neuron) {
       outputNeurons.Add(neuron);
     }
-
     //set input
     public void SetInput(double[] vals) {
-      if (inputNeurons.Count != vals.Length)
+      if(inputNeurons.Count != vals.Length)
         throw new Exception("Size of input doesn't match number of input neurons.");
-      for (int i = 0; i < vals.Length; i++)
+      for(int i = 0; i < vals.Length; i++)
         inputNeurons[i].SetValue(vals[i]);
     }
-
     //calculate output
     public double[] GetOutput() {
       //calculate value of hidden neurons
-      for (int i = 0; i < hiddenNeurons.Count; i++)
+      for(int i = 0; i < hiddenNeurons.Count; i++)
         hiddenNeurons[i].Calculate();
 
       //calculate value of output neurons
@@ -86,7 +113,6 @@ namespace ArtificialNeuralNetwork {
       }
       return output;
     }
-
   }
 }
 
