e2c21fe2bdc0e553020351ca67d73953730cafe7
Elias Obeid <eobeid11@student.aau.dk>
2014-02-24 17:29:18 +0100

    Comments included.

--- Project/Program/GANNAI/NeuralNetwork/NeuralNetwork.cs
+++ Project/Program/GANNAI/NeuralNetwork/NeuralNetwork.cs
@@ -3,109 +3,158 @@ using System.Text;
 using System.Collections.Generic;
 
 namespace ArtificialNeuralNetwork {
+  /// <summary>
+  /// Neural network.
+  /// </summary>
   public class NeuralNetwork {
-    //list of neurons in network
+    /// <summary>
+    /// The input neurons.
+    /// </summary>
     private List<InputNeuron> inputNeurons;
+    /// <summary>
+    /// The hidden neurons.
+    /// </summary>
     private List<ChildNeuron> hiddenNeurons;
+    /// <summary>
+    /// The output neurons.
+    /// </summary>
     private List<ChildNeuron> outputNeurons;
 
+    /// <summary>
+    /// Initializes a new instance of the 
+    /// <see cref="ArtificialNeuralNetwork.NeuralNetwork"/> class.
+    /// </summary><param name="inp">Number of input neurons to be built.</param>
+    /// <param name="hdn">Number of hidden neurons to be built.</param>
+    /// <param name="oup">Number of output neurons to be built.</param>
     public NeuralNetwork(int inp, int hdn, int oup) {
       InitiateNetwork(inp, hdn, oup);
       AddConnections();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the 
+    /// <see cref="ArtificialNeuralNetwork.NeuralNetwork"/> class.
+    /// </summary>
+    /// <param name="inp">Number of input neurons to be built.</param>
+    /// <param name="hdn">Number of hidden neurons to be built.</param>
+    /// <param name="oup">Number of output neurons to be built.</param>
+    /// <param name="weights">Weights to be distributed in the network.</param>
     public NeuralNetwork(int inp, int hdn, int oup, double[] weights) {
       InitiateNetwork(inp, hdn, oup);
       AddConnections(weights);
     }
 
-    private void InitiateNetwork(int inp, int hdn, int oup){
+    /// <summary>
+    /// Initiates the network. No connections added yet.
+    /// </summary>
+    /// <param name="inp">Number of input neurons to be built.</param>
+    /// <param name="hdn">Number of hidden neurons to be built.</param>
+    /// <param name="oup">Number of output neurons to be built.</param>
+    private void InitiateNetwork(int inp, int hdn, int oup) {
       inputNeurons = new List<InputNeuron>();
       hiddenNeurons = new List<ChildNeuron>();
       outputNeurons = new List<ChildNeuron>();
 
-      //validation
       if(inp < 0)
         throw new Exception("Must have at least one input neuron!"
-          + inp + " input neurons where given.");
+        + inp + " input neurons where given.");
       if(hdn < 0)
         throw new Exception("Must have at least one hidden neuron!"
-          + hdn + " hidden neurons where given.");
+        + hdn + " hidden neurons where given.");
       if(oup < 0)
         throw new Exception("Must have at least one output neuron!"
-          + oup + " output neurons where given.");
+        + oup + " output neurons where given.");
 
-      //add input neurons to network
       for(int i = 0; i < inp; i++)
         AddInput(new InputNeuron());
-      //add hidden neurons to network
       for(int i = 0; i < hdn; i++)
         AddHidden(new ChildNeuron());
-      //add output neurons to network
       for(int i = 0; i < oup; i++)
         AddOutput(new ChildNeuron());
     }
 
-    private void AddConnections(){
-      //connect network (input to hidden)
-      for(int i = 0; i < hiddenNeurons.Count; i++) {
-        for(int j = 0; j < inputNeurons.Count; j++) {
-          hiddenNeurons[j].AddInputConnection(inputNeurons[i]);
+    /// <summary>
+    /// Adds the connections in the network.
+    /// Begins with input neurons, then hidden and finally output neurons.
+    /// Weights are distributed in that order.
+    /// </summary>
+    /// <param name="weights">Weights to be distributed in the network.
+    /// Default is null, which leads to random values.</param>
+    private void AddConnections(double[] weights = null) {
+      if(weights == null) {
+        for(int i = 0; i < hiddenNeurons.Count; i++) {
+          for(int j = 0; j < inputNeurons.Count; j++) {
+            hiddenNeurons[j].AddInputConnection(inputNeurons[i]);
+          }
+        }
+        for(int i = 0; i < outputNeurons.Count; i++) {
+          for(int j = 0; j < hiddenNeurons.Count; j++) {
+            outputNeurons[i].AddInputConnection(hiddenNeurons[j]);
+          }
         }
       }
-      //connect network (hidden to output)
-      for(int i = 0; i < outputNeurons.Count; i++) {
-        for(int j = 0; j < hiddenNeurons.Count; j++) {
-          outputNeurons[i].AddInputConnection(hiddenNeurons[j]);
+      else {
+        if(weights.Length != (inputNeurons.Count * hiddenNeurons.Count)
+           + (hiddenNeurons.Count * outputNeurons.Count))
+          throw new Exception("Number of weights do not correspond to number of connections.");
+
+        int weightIndex = 0;
+        for(int i = 0; i < hiddenNeurons.Count; i++) {
+          for(int j = 0; j < inputNeurons.Count; j++) {
+            hiddenNeurons[i].AddInputConnection(inputNeurons[j], weights[weightIndex++]);
+          }
+        }
+        for(int i = 0; i < outputNeurons.Count; i++) {
+          for(int j = 0; j < hiddenNeurons.Count; j++) {
+            outputNeurons[i].AddInputConnection(hiddenNeurons[j], weights[weightIndex++]);
+          }
         }
       }
     }
 
-    private void AddConnections(double[] weights) {
-      if(weights.Length != (inputNeurons.Count * hiddenNeurons.Count) 
-        + (hiddenNeurons.Count * outputNeurons.Count))
-        throw new Exception("Number of weights do not correspond to number of connections.");
-
-      //connect network (input to hidden)
-      int weightIndex = 0;
-      for(int i = 0; i < hiddenNeurons.Count; i++) {
-        for(int j = 0; j < inputNeurons.Count; j++) {
-          hiddenNeurons[i].AddInputConnection(inputNeurons[j], weights[weightIndex++]);
-        }
-      }
-      //connect network (hidden to output)
-      for(int i = 0; i < outputNeurons.Count; i++) {
-        for(int j = 0; j < hiddenNeurons.Count; j++) {
-          outputNeurons[i].AddInputConnection(hiddenNeurons[j], weights[weightIndex++]);
-        }
-      }
-    }
-    //add input neuron
+    /// <summary>
+    /// Adds an input neuron.
+    /// </summary>
+    /// <param name="neuron">Input neuron.</param>
     public void AddInput(InputNeuron neuron) {
       inputNeurons.Add(neuron);
     }
-    //add hidden neuron
+
+    /// <summary>
+    /// Adds a hidden neuron.
+    /// </summary>
+    /// <param name="neuron">Output neuron.</param>
     public void AddHidden(ChildNeuron neuron) {
       hiddenNeurons.Add(neuron);
     }
-    //add output neuron
+
+    /// <summary>
+    /// Adds an output neuron.
+    /// </summary>
+    /// <param name="neuron">Output neuron.</param>
     public void AddOutput(ChildNeuron neuron) {
       outputNeurons.Add(neuron);
     }
-    //set input
+
+    /// <summary>
+    /// Sets the input vector.
+    /// </summary>
+    /// <param name="vals">The input vector.</param>
     public void SetInput(double[] vals) {
       if(inputNeurons.Count != vals.Length)
         throw new Exception("Size of input doesn't match number of input neurons.");
       for(int i = 0; i < vals.Length; i++)
         inputNeurons[i].SetValue(vals[i]);
     }
-    //calculate output
+
+    /// <summary>
+    /// Gets the output.
+    /// </summary>
+    /// <returns>The output as an array.</returns>
     public double[] GetOutput() {
-      //calculate value of hidden neurons
       for(int i = 0; i < hiddenNeurons.Count; i++)
         hiddenNeurons[i].Calculate();
-
-      //calculate value of output neurons
+        
       double[] output = new double[outputNeurons.Count];
       for(int i = 0; i < outputNeurons.Count; i++) {
         outputNeurons[i].Calculate();
--- Project/Program/GANNAI/NeuralNetwork/Neuron.cs
+++ Project/Program/GANNAI/NeuralNetwork/Neuron.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 using GANNAI;
 
 namespace ArtificialNeuralNetwork {
+  /// <summary>
+  /// Base neuron.
+  /// </summary>
   public class Neuron {
 
-    //neuron value
+    /// <summary>
+    /// Gets or sets the value.
+    /// </summary>
+    /// <value>The value.</value>
     public double Value { get; protected set; }
   }
 }
