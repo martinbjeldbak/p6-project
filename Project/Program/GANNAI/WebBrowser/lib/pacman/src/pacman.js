a2e23d3cefe528136640a1cedb2ee47347294e99
Martin Madsen <martin2madsen@gmail.com>
2014-02-26 15:47:34 +0100

    Hook into pacman.js files to write to web server upon game state changing

---
+++ Project/Program/GANNAI/WebBrowser/lib/pacman/src/pacman.js
@@ -0,0 +1,57 @@
+
+// Copyright 2012 Shaun Williams
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License Version 3 as 
+//  published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+// ==========================================================================
+// PAC-MAN
+// an accurate remake of the original arcade game
+
+// Based on original works by Namco, GCC, and Midway.
+// Research by Jamey Pittman and Bart Grantham
+// Developed by Shaun Williams
+
+// ==========================================================================
+
+(function(){
+
+//@line 1 "src/inherit.js"
+//@line 1 "src/random.js"
+//@line 1 "src/game.js"
+//@line 1 "src/direction.js"
+//@line 1 "src/Map.js"
+//@line 1 "src/colors.js"
+//@line 1 "src/mapgen.js"
+//@line 1 "src/atlas.js"
+//@line 1 "src/renderers.js"
+//@line 1 "src/hud.js"
+//@line 1 "src/galagaStars.js"
+//@line 1 "src/Button.js"
+//@line 1 "src/Menu.js"
+//@line 1 "src/inGameMenu.js"
+//@line 1 "src/sprites.js"
+//@line 1 "src/Actor.js"
+//@line 1 "src/Ghost.js"
+//@line 1 "src/Player.js"
+//@line 1 "src/actors.js"
+//@line 1 "src/targets.js"
+//@line 1 "src/ghostCommander.js"
+//@line 1 "src/ghostReleaser.js"
+//@line 1 "src/elroyTimer.js"
+//@line 1 "src/energizer.js"
+//@line 1 "src/fruit.js"
+//@line 1 "src/executive.js"
+//@line 1 "src/states.js"
+//@line 1 "src/input.js"
+//@line 1 "src/cutscenes.js"
+//@line 1 "src/maps.js"
+//@line 1 "src/vcr.js"
+//@line 1 "src/main.js"
+})();
