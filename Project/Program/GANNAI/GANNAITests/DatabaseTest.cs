0eb25e87480908aee230c3fb079f86ab3a939174
Martin Madsen <martin2madsen@gmail.com>
2014-03-04 14:09:55 +0100

    Add Simple.Data and npgsql packages

---
+++ Project/Program/GANNAI/GANNAITests/DatabaseTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using GANNAI;
+
+namespace GANNAITests {
+  [TestFixture()]
+  public class DatabaseTest {
+    [Test()]
+    public void TestCase() {
+      ChildNeuronDB cnd = new ChildNeuronDB();
+    }
+  }
+}
+
