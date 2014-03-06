939bed5b8b84499becc34ee402a989c074f6c7a5
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-03-06 13:58:00 +0100

    Add static logging class to util

---
+++ Project/Program/GANNAI/Utility/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Utility {
+  public static class Logger {
+    public static void Log(string msg) {
+      LogMessage(msg, "info");
+    }
+
+    public static void Debug(string msg) {
+      LogMessage(msg, "debug");
+    }
+
+    public static void Error(string msg) {
+      LogMessage(msg, "error");
+    }
+
+    private static void LogMessage(string msg, string type) {
+      StreamWriter w = File.AppendText("log.txt");
+
+      // Write header
+      w.WriteLine("{0} {1}: {2}", DateTime.Now.ToString("g"), type.ToUpper(), msg);
+        
+      w.Close();
+    }
+  }
+}
+
