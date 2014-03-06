8012469f4a7d87c526669d6c57c1bc4493bfef22
Kent Caspersen <kcaspe11@student.aau.dk>
2014-03-06 14:10:08 +0100

    rettet få fejl

--- Project/Program/GANNAI/GANNAI-UI/Program.cs
+++ Project/Program/GANNAI/GANNAI-UI/Program.cs
@@ -11,7 +11,7 @@ namespace GANNAIUI {
     /// </summary>
     [STAThread]
     static void Main() {
-      Logger.Log("Starting GANNAI-UI");
+      Log.Info("Starting GANNAI-UI");
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new Form1());
