53ef900d0aabd753a767a26d4bd8cd83b917ad89
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-03-24 23:21:56 +0100

    Refactoring, add adult income dataset to resources

--- Project/Program/GANNAI/Datasets/Dataset.cs
+++ Project/Program/GANNAI/Datasets/Dataset.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace Datasets {
   public abstract class Dataset {
     public double ValidationPercentage { get; private set; }
+    public List<List<string>> DataSet { get; private set;  }
     public List<List<string>> ValidationSet { get; private set; }
     public List<List<string>> TestSet { get; private set; }
     public double TestPercentage {
@@ -20,13 +21,23 @@ namespace Datasets {
     /// </summary>
     /// <param name="validation">Percentage of dataset to be used as validation. Range 0-1.</param>
     protected Dataset(double validationPercent = 0.4) {
+      DataSet = new List<List<string>>();
       ValidationSet = new List<List<string>>();
       TestSet = new List<List<string>>();
       this.ValidationPercentage = validationPercent;
     }
 
-    protected void CreateValidationAndTestSet(List<List<string>> dataSet) {
-      List<List<string>> shuffledData = dataSet.OrderBy(i => Guid.NewGuid()).ToList();
+    /// <summary>
+    /// Gets the indicies of the row(s) in the data set that
+    /// have to be guessed based on intput
+    /// </summary>
+    /// <returns></returns>
+    public abstract int[] OutputIndicies();
+
+    public abstract int[] InputIndicies();
+
+    protected void CreateValidationAndTestSet() {
+      List<List<string>> shuffledData = DataSet.OrderBy(i => Guid.NewGuid()).ToList();
 
       // Get the amount of items we need from the dataset to put in our validation set
       int validationCount = (int)Math.Floor(shuffledData.Count * ValidationPercentage);
--- Project/Program/GANNAI/Datasets/Properties/Resources.Designer.cs
+++ Project/Program/GANNAI/Datasets/Properties/Resources.Designer.cs
@@ -61,6 +61,19 @@ namespace Datasets.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to 39, State-gov, 77516, Bachelors, 13, Never-married, Adm-clerical, Not-in-family, White, Male, 2174, 0, 40, United-States, &lt;=50K
+        ///50, Self-emp-not-inc, 83311, Bachelors, 13, Married-civ-spouse, Exec-managerial, Husband, White, Male, 0, 0, 13, United-States, &lt;=50K
+        ///38, Private, 215646, HS-grad, 9, Divorced, Handlers-cleaners, Not-in-family, White, Male, 0, 0, 40, United-States, &lt;=50K
+        ///53, Private, 234721, 11th, 7, Married-civ-spouse, Handlers-cleaners, Husband, Black, Male, 0, 0, 40, United-States, &lt;=50K
+        ///28, Pri [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string adult {
+            get {
+                return ResourceManager.GetString("adult", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to 5.1,3.5,1.4,0.2,Iris-setosa
         ///4.9,3.0,1.4,0.2,Iris-setosa
         ///4.7,3.2,1.3,0.2,Iris-setosa
