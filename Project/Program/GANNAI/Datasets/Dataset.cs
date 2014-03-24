1b520275737c25241b14675c30081b7b26a80d27
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-03-24 13:49:07 +0100

    Minor refractor in Iris dataset. Accidentally swapped validation with test set

--- Project/Program/GANNAI/Datasets/Dataset.cs
+++ Project/Program/GANNAI/Datasets/Dataset.cs
@@ -8,20 +8,21 @@ namespace Datasets {
     public double ValidationPercentage { get; private set; }
     public List<List<string>> ValidationSet { get; private set; }
     public List<List<string>> TestSet { get; private set; }
+    public double TestPercentage {
+      get {
+        return 1 - this.ValidationPercentage;
+      }
+    }
 
     /// <summary>
     /// Creates a new dataset with the given percentage of
     /// the dataset denoted as the valdiation set
     /// </summary>
     /// <param name="validation">Percentage of dataset to be used as validation. Range 0-1.</param>
-    protected Dataset(double validationPercent = 0.6) {
+    protected Dataset(double validationPercent = 0.4) {
       ValidationSet = new List<List<string>>();
       TestSet = new List<List<string>>();
       this.ValidationPercentage = validationPercent;
-    }
-
-    public double TestPercentage() {
-      return 1 - ValidationPercentage;
     }
 
     protected void CreateValidationAndTestSet(List<List<string>> dataSet) {
