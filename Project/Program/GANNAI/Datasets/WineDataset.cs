2a1be6e3fcdad0f22973493632a997e1d8ac4169
Elias Obeid <eobeid11@student.aau.dk>
2014-04-04 12:45:58 +0200

    wine is done

---
+++ Project/Program/GANNAI/Datasets/WineDataset.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Datasets {
+  public class WineDataset : Dataset {
+    public WineDataset() : base() {
+      string csv = Properties.Resources.wine;
+
+      DataSet.AddRange(ParseDataFromString(csv));
+      CreateMappedDataset();
+    }
+
+    #region implemented abstract members of Dataset
+
+    public override int[] OutputIndicies() {
+      return new int[] {
+        0 //Alcohol   
+      };
+    }
+
+    public override int[] InputIndicies() {
+      return new int[] {        
+        1,  //Malic acid 
+        2,  //Ash 
+        3,  //Alcalinity of ash 
+        4,  //Magnesium 
+        5,  //Total phenols 
+        6,  //Flavanoids 
+        7,  //Nonflavanoid phenols 
+        8,  //Proanthocyanins 
+        9,  //Color intensity 
+        10, //Hue 
+        11, //OD280/OD315 of diluted wines 
+        12, //Proline 
+      };
+    }
+
+    protected override void CreateMappedDataset() {
+      foreach(List<string> row in DataSet) {
+        int columns = row.Count;
+        
+        Line data = new Line();
+        MappedDataSet.Add(data);
+        
+        data.AddEntry(new LineEntry(row[0], Double.Parse(row[0]), 0));
+        
+        for(int i = 1; i < columns; i++) {
+          if(i > 12)
+            throw new Exception("The column is out of bounds. "
+              + "The leaf dataset contains " + 13 + " columns. "
+              + "Tried to access the " + (i+1) + "th column!");
+          else
+            data.AddEntry(new LineEntry("column " + i, Double.Parse(row[i], CultureInfo.InvariantCulture), i));
+        }
+      }
+    }
+
+    #endregion
+  }
+}
+
---
+++ Project/Program/GANNAI/Games/Wine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Genetics;
+using System.Collections.Generic;
+using Datasets;
+using System.Linq;
+
+namespace Games {
+  public class Wine : AITrainableGame  {
+    WineDataset wine;
+    
+    public Wine() {
+      this.wine = new WineDataset();
+    }
+
+    #region AITrainableGame implementation
+
+    public double CalcFitness(AIPlayer aiplayer) {
+      int fitness = 0;
+      int numInputs = NumInputs();
+      int outputIndex = 0;
+      List<double> inputs;
+      
+      foreach(Line row in wine.MappedDataSet) {
+        inputs = new List<double>(numInputs);
+        inputs.AddRange(row.entries.Where(e => e.Column > 0).Select(e => e.Value));
+        
+        int outputNeuron = aiplayer.GetStrongestOutputIndex(inputs.ToArray());
+        if(outputNeuron == row.entries[outputIndex].Value)
+          fitness++;
+      }
+      return fitness;
+    }
+
+    public int NumInputs() {
+      return wine.InputIndicies().Count();
+    }
+
+    public int NumOutputs() {
+      return 3;
+    }
+
+    public AITrainableGame GetNewGameInstance() {
+      throw new NotImplementedException();
+    }
+
+    public void Visualize(AIPlayer aiplayer, System.Windows.Forms.Form form) {
+      throw new NotImplementedException();
+    }
+
+    public string Name() {
+      return "Wine Classification";
+    }
+
+    #endregion
+  }
+}
+
