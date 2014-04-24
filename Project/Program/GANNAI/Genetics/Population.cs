51202639887b984b6dcf4d946b8f390d9c252929
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-04-24 13:34:32 +0200

    Refactor diversity measures to allow multiple measures

--- Project/Program/GANNAI/Genetics/Population.cs
+++ Project/Program/GANNAI/Genetics/Population.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -21,11 +20,6 @@ namespace Genetics {
             Generation = generation;
             individuals = new SortList<AIPlayer>();
             InitializeRandomPopulation();
-        }
-
-        public int TotalIndividuals() {
-            return 0;
-            // Return the total amount of total individual, which this generation * population size
         }
 
         /// <summary>
@@ -184,39 +178,8 @@ namespace Genetics {
         /// <returns>The diversity.</returns>
         /// <param name="runs">Number of runs.</param>
         public double MeasureDiversity(int runs = 100) {
-            int outputSize = individuals.Get(0).neuralNetwork.GetNumberOfOutputs();
-
-            double[] diversities = new double[runs];
-
-            for (int j = 0; j < runs; j++) {
-                int[] outputCount = new int[outputSize];
-                double[] randInputs = new double[individuals.Get(0).neuralNetwork.GetNumberOfInputs()];
-                for (int i = 0; i < randInputs.Length; i++)
-                    randInputs[i] = RandomNum.RandomDouble();
-
-                Parallel.For(0, individuals.Count, i => {
-                    AIPlayer a = individuals.Get(i);
-                    outputCount[a.GetStrongestOutputIndex(randInputs)]++;
-                    double[] outputs = a.GetOutputs(randInputs);
-                });
-
-                double numerator = 0.0;
-                int totalOrganisms = 0;
-                for (int i = 0; i < outputSize; i++) {
-                    numerator += outputCount[i] * (outputCount[i] - 1);
-                    totalOrganisms += outputCount[i];
-                }
-
-                int s = individuals.Count;
-                double demoninator = totalOrganisms * (totalOrganisms - 1);
-                diversities[j] = 1.0 - numerator / demoninator;
-            }
-
-            double diversity = 0.0;
-            for (int i = 0; i < runs; i++)
-                diversity += diversities[i];
-
-            return diversity / runs;
-        }
+      //Simulation.offspringMerger.Merge(individuals, offspring, Simulation);
+      return Simulation.diversityMeasure.MeasureDiversity(individuals, runs);
     }
+  }
 }
